e0cbbc9668fabc1be47eaafd2c3e0842a8587857
Thalita Alves de Freitas <thalitaalvesf@gmail.com>
2020-02-26 23:22:44 -0300

    MOVIE-DB - Favorite Movie'
    q

--- app/src/main/java/com/thalita/movie_db_app/ui/activities/MovieDetailsActivity.kt
+++ app/src/main/java/com/thalita/movie_db_app/ui/activities/MovieDetailsActivity.kt
@@ -1,6 +1,7 @@
 package com.thalita.movie_db_app.ui.activities
 
 import android.os.Build
+import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -19,6 +20,7 @@ class MovieDetailsActivity : GenericActivity() {
     private lateinit var movieAverage: TextView
     private lateinit var moviePopular: TextView
     private lateinit var movieOverview: TextView
+    private lateinit var favoriteMovie: CheckBox
 
     override fun setLayout() {
         hideTop(true)
@@ -26,6 +28,7 @@ class MovieDetailsActivity : GenericActivity() {
     }
 
     override fun getObjects() {
+
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -41,18 +44,19 @@ class MovieDetailsActivity : GenericActivity() {
         movieAverage = findViewById(R.id.tv_movie_average)
         moviePopular = findViewById(R.id.tv_movie_popular)
         movieOverview = findViewById(R.id.tv_movie_overview)
+        favoriteMovie = findViewById(R.id.checkbox_favorite)
 
         setMovieDetails()
+//        setMovieAsFavorite()
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun setMovieDetails() {
-        val response: MovieResult.MovieResponse=
-            intent.getSerializableExtra("movieDetails") as MovieResult.MovieResponse
-
-        val posterURL= "https://image.tmdb.org/t/p/w500" + response.poster_path
+        val response: MovieResult.MovieResponse = intent.getSerializableExtra("movieDetails") as MovieResult.MovieResponse
+        val posterURL= "https://image.tmdb.org/t/p/w800" + response.poster_path
         val linearLayout = findViewById<LinearLayout>(R.id.linear_image)
         val imageView = ImageView(this)
+
         Glide.with(this).load(posterURL).into(imageView)
         linearLayout.addView(imageView)
 
@@ -68,5 +72,15 @@ class MovieDetailsActivity : GenericActivity() {
         movieAverage.text = response.vote_average
         moviePopular.text = response.popularity
         movieOverview.text = response.overview
+    }
+
+    private fun setMovieAsFavorite(){
+        favoriteMovie.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                favoriteMovie.text = "Adicinado aos favoritos"
+            }else{
+                favoriteMovie.text = "Marcar como favorito"
+            }
+        }
     }
 }
